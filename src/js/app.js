34987f458b7dab03e3faa1117aa8a9dbf9a76a98
Camoralesh27 <89227331+Camoralesh27@users.noreply.github.com>
2024-12-08 13:52:03 -0600

    envio de datos del forms al sheets

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,37 @@ const showMenu = (toggleId, navId) =>{
  }
  
  showMenu('navM-toggle','navM-menu');
+
+
+ /* ==== DATOS DE FORM A SHEETS ==== */
+ const scriptURL = 'https://script.google.com/macros/s/AKfycbwg35rfT3xEbPixWQTenjz8vMZDBnFgCaDXano1a4WrPzFByt8-xafUkB-xvTVJT3GKhw/exec';
+
+ const form = document.forms['contact-form'];
+ 
+ form.addEventListener('submit', e => {
+     e.preventDefault(); // Evita que el formulario recargue la página
+     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+         .then(response => {
+             if (response.ok) {
+                 alert("¡Gracias por contactarnos! Tus datos han sido enviados. Un asesor se pondrá en contacto contigo.");
+                 form.reset(); // Limpia el formulario después del envío
+             } else {
+                 throw new Error('Error al enviar los datos.');
+             }
+         })
+         .catch(error => console.error('Error al enviar el formulario:', error.message));
+ });
+ 
+
+
+
+/* const scriptURL = 'https://script.google.com/macros/s/AKfycbwg35rfT3xEbPixWQTenjz8vMZDBnFgCaDXano1a4WrPzFByt8-xafUkB-xvTVJT3GKhw/exec';
+
+const form = document.forms['contact-form'];
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL,{ method: 'POST', body: new FormData(form)})
+    .then(response => alert("¡Gracias por contactarnos! Tus datos han sido enviados. Un asesor se pondrá en contacto contigo."))
+    .then(error => console.error('Error!', error.message));
+} */
